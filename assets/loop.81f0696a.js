7ddd1cbf82641e28215583540dc5fe377c7c346c
aizcutei <aiz.cutei@gmail.com>
2022-09-24 05:14:16 +0000

    Deploying to gh-pages from @ aizcutei/aizcutei.github.io@4507e0219507a4758f57108cc433861dc36f6bfa 🚀

---
+++ assets/loop.81f0696a.js
@@ -0,0 +1,1 @@
+import{w as m}from"./writeAndUnwriteText.bf806bb0.js";import{a as s,m as r}from"./animationSetup.9a8f1137.js";import"./writeEffect.94a3b022.js";import"./unwriteEffect.e0399bd9.js";import"./index.ea33571e.js";const w=async(t,e)=>{const{options:o,elements:i}=s(t,e);for(;;){r(t);for(const a of i)await m(a,o)}};export{w as default};
