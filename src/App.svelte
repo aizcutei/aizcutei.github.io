c057c5bac08b8bfe1dc50b00b0c52f5215446b02
Charles Zhang <aiz.cutei@gmail.com>
2022-09-23 23:21:34 -0500

    init repo

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Avatar from "./assets/avatar.svg";
+  import { Router, Route, Link } from "svelte-navigator";
+  import Banner from "./lib/Banner.svelte";
+  import About from "./lib/About.svelte";
+  import Projects from "./lib/Projects.svelte";
+  import Works from "./lib/works.svelte";
+
+  function handleMousemove(event) {}
+</script>
+
+<Router>
+  <header>
+    <div />
+  </header>
+  <main>
+    <Route path="/">
+      <Banner />
+    </Route>
+
+    <Route path="/about">
+      <About />
+    </Route>
+
+    <Route path="/projects">
+      <Projects />
+    </Route>
+
+    <Route path="/works">
+      <Works />
+    </Route>
+
+    <nav>
+      <Link to="/" class="px-4 text-blue font-500 decoration-none">Home</Link>
+      <Link to="about" class="px-4 text-blue font-500 decoration-none"
+        >About</Link
+      >
+      <Link to="projects" class="px-4 text-blue font-500 decoration-none"
+        >Projects</Link
+      >
+      <Link to="works" class="px-4 text-blue font-500 decoration-none"
+        >Works</Link
+      >
+    </nav>
+  </main>
+</Router>
+<img
+  src={Avatar}
+  alt="Avatar"
+  class="absolute right-0 bottom-0 h-8 w-8 object-cover hover:h-12 hover:w-12 op90"
+  on:click={() => {
+    window.open("https://www.github.com/aizcutei");
+  }}
+/>
+
+<style>
+</style>
---
+++ src/lib/Projects.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { onMount } from "svelte";
+  import { fade, slide, fly } from "svelte/transition";
+  import { expoInOut, expoOut } from "svelte/easing";
+  import Typewriter from "svelte-typewriter";
+  let visible = false;
+
+  onMount(() => {
+    setTimeout(() => {
+      visible = true;
+    }, 1000);
+  });
+</script>
+
+{#if visible}
+  <div class="m-10" transition:slide={{ duration: 500, easing: expoInOut }}>
+    <div
+      id="projects"
+      class="bg-[#6edcff] h-60 rounded-lg rotate-0 mx-auto max-w-lg"
+    >
+      <Typewriter mode="cascade">
+        <div class="text-white text-lg font-800 p-5">
+          <p>Sorry</p>
+          <p>This page is under constraction.</p>
+          <p>Please come back later.</p>
+        </div>
+      </Typewriter>
+    </div>
+  </div>
+{/if}
+
+<style>
+  #projects {
+    background: rgb(148, 227, 254);
+    background: linear-gradient(
+      45deg,
+      rgba(148, 227, 254, 1) 37%,
+      rgba(174, 187, 217, 1) 100%
+    );
+
+    box-shadow: 1px -1px 10px rgba(174, 187, 217, 1), -1px 1px 10px #aaffff;
+
+    transition: 300ms;
+  }
+  #projects:hover {
+    box-shadow: 1px -1px 20px #7ec1d8, -1px 1px 20px #aaffff;
+  }
+</style>
