941f8f82e5dfb975e28164b9691cf577ddfaea15
aizcutei <aiz.cutei@gmail.com>
2023-07-19 23:24:15 -0500

    renew

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,29 @@
 <script>
-  import Avatar from "./assets/avatar.svg";
-  import { Router, Route, Link } from "svelte-navigator";
-  import Banner from "./lib/Banner.svelte";
-  import About from "./lib/About.svelte";
-  import Projects from "./lib/Projects.svelte";
-  import Works from "./lib/works.svelte";
 
-  function handleMousemove(event) {}
 </script>
 
-<Router>
-  <header>
-    <div />
-  </header>
-  <main>
-    <Route path="/">
-      <Banner />
-    </Route>
 
-    <Route path="/about">
-      <About />
-    </Route>
+<header>
+</header>
+<main>
+  <h1>under maintenance</h1>
+  <h2>Please come back soon!</h2>
+</main>
 
-    <Route path="/projects">
-      <Projects />
-    </Route>
 
-    <Route path="/works">
-      <Works />
-    </Route>
-
-    <nav>
-      <Link to="/" class="px-4 text-blue font-500 decoration-none">Home</Link>
-      <Link to="about" class="px-4 text-blue font-500 decoration-none"
-        >About</Link
-      >
-      <Link to="projects" class="px-4 text-blue font-500 decoration-none"
-        >Projects</Link
-      >
-      <Link to="works" class="px-4 text-blue font-500 decoration-none"
-        >Works</Link
-      >
-    </nav>
-  </main>
-</Router>
-<img
-  src={Avatar}
-  alt="Avatar"
-  class="absolute right-0 bottom-0 h-8 w-8 object-cover hover:h-12 hover:w-12 op90"
-  on:click={() => {
-    window.open("https://www.github.com/aizcutei");
-  }}
-/>
 
 <style>
+  main {
+    font-family: 'GB';
+    color: cornsilk;
+    animation: shine 2s infinite alternate;
+	}
+  @keyframes shine{
+    0%{
+      text-shadow: white 0px 0px 10px;
+    }
+    100%{
+      text-shadow: white 0px 0px 0px;
+    }
+  }
 </style>
